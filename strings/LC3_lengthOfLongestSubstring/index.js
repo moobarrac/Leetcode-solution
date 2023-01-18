5dc03c159c99e4aa561151165974e86b26641821
moobarrac <wahabajibola01@gmail.com>
2023-01-18 11:37:25 +0100

    solution for length of longest substring

--- strings/LC3_lengthOfLongestSubstring/index.js
+++ strings/LC3_lengthOfLongestSubstring/index.js
@@ -1,3 +1,19 @@
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  let start = 0;
+    let end = 0;
+    let maxLength = 0;
+    let substring = new Set();
+    while (end < s.length) {
+        if(substring.has(s[end])) {
+            substring.delete(s[start]);
+            start++;
+        } else {
+            substring.add(s[end]);
+            end++;
+            maxLength = Math.max(maxLength, end - start)
+        }
+    }
+    return maxLength;
+}
 
 module.exports = lengthOfLongestSubstring;
