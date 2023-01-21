418f5a1faf3441f6db0c51ac2daf72e91acc3240
moobarrac <wahabajibola01@gmail.com>
2023-01-21 22:35:41 +0100

    valid parenthesis

--- strings/LC20_isValid/index.js
+++ strings/LC20_isValid/index.js
@@ -1,3 +1,19 @@
-function isValid(s) {}
+function isValid(s) {
+  let stack = [];
+  for (i = 0; i < s.length; i++) {
+    if (s[i] === "{" || s[i] === "[" || s[i] === "(") {
+      stack.push(s[i]);
+    } else if (s[i] === '}' && stack.length !== 0 && stack[stack.length - 1] === '}') {
+      stack.pop()
+    } else if (s[i] === ']' && stack.length !== 0 && stack[stack.length - 1] === ']') {
+      stack.pop()
+    } else if (s[i] === ')' && stack.length !== 0 && stack[stack.length - 1] === ')') {
+      stack.pop()
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
+}
 
 module.exports = isValid;
